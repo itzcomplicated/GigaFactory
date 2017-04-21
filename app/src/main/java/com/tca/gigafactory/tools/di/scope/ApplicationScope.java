945bf23823bf4cd0abfc0bc91d7617b4dc037b97
TCA <arun.cthomas3@gmail.com>
2017-04-21 23:47:03 +0530

    Creating new Scopes

---
+++ app/src/main/java/com/tca/gigafactory/tools/di/scope/ApplicationScope.java
@@ -0,0 +1,14 @@
+package com.tca.gigafactory.tools.di.scope;
+
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+
+import javax.inject.Scope;
+
+/**
+ * Created by TCA on 21-04-2017.
+ */
+@Scope
+@Retention(RetentionPolicy.CLASS)
+public @interface ApplicationScope {
+}
